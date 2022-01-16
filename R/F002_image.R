5d1f3b03b59bae12412f7e18a5cb3de88e404366
R-graphic-design <30767037+R-graphic-design@users.noreply.github.com>
2022-01-16 15:29:36 +0000

    Fix makeImage
    Remove image parameter from makeImage for greater generality

--- R/F002_image.R
+++ R/F002_image.R
@@ -2,8 +2,7 @@
 #'
 #' @description
 #' Functions to make a component for the rasterImage function in the base graphics system.
-#' makeImage assumes the input is an RGB array, whilst makeImageFromMatrix converts a matrix into colours for you using matrixToColours.
-#' @param image a matrix or RGB array
+#' makeImage assumes the input "image"is an RGB array, whilst makeImageFromMatrix converts a matrix into colours for you using matrixToColours.
 #' @param type plotting function(s) used
 #' @param xleft,xright,ytop,ybottom positioning. see rasterImage
 #' @param ... parameters passed to the component or the component functions imageColourScheme and matrixToColours.
@@ -14,8 +13,8 @@ NULL
 
 #' @rdname makeImage
 #' @export
-makeImage=function(image,type="rasterImage",xleft=0,xright=1,ytop=1,ybottom=0,...){
-	return(component(image=image,xleft=xleft,xright=xright,ytop=ytop,ybottom=ybottom,...))
+makeImage=function(type="rasterImage",xleft=0,xright=1,ytop=1,ybottom=0,...){
+	return(component(xleft=xleft,xright=xright,ytop=ytop,ybottom=ybottom,...))
 }
 #' @rdname makeImage
 #' @export
